056c5aa0766956d1773220165c4c0c5b09769b3d
Emran Khandaker Evan <evanemran007@gmail.com>
2023-01-10 08:44:46 +0600

    init

---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,62 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Camora</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+    <ion-fab-button @click="takePhoto()">
+      <ion-icon :icon="camera"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {  IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+
+export default {
+  name: 'Tab2',
+  components: {
+    IonPage,
+    IonHeader,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  },
+  setup() {
+    return {
+      camera,
+      trash,
+      close,
+    };
+  },
+};
+
+</script>
